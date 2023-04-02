db6dd86df82862f484296eb9a2f1c9012471c9d8
MatheusPK <matheuskulick@hotmail.com>
2023-04-02 20:50:40 -0300

    [ADD] Latency observer for client

--- LoveNetwork/client.lua
+++ LoveNetwork/client.lua
@@ -13,18 +13,28 @@ function Client:init(server_address, server_port, id)
     self.pending_messages = {}
     self.current_sequence_number = 0
     self.event_triggers = {}
+    self.latency = 0
+end
+
+function Client:set_latency()
+    self:on('latency', function (data)
+        self.latency = math.floor((socket.gettime() - data.content.send_time) * 1000)
+    end)
 end
 
 function Client:send(event, content)
-    self.current_sequence_number = self.current_sequence_number + 1
-
     local message = {}
     message.sender = self.id
     message.event = event
     message.content = content
-    message.sequence_number = self.current_sequence_number
 
-    table.insert(self.pending_messages, message)
+    if event == 'latency' or event ~= 'connect' then
+        message.sequence_number = 0
+    else
+        self.current_sequence_number = self.current_sequence_number + 1
+        message.sequence_number = self.current_sequence_number
+        table.insert(self.pending_messages, message)
+    end
 
     local encoded_message = utils:encode(message)
     self.socket:send(encoded_message)
@@ -51,10 +61,18 @@ function Client:update()
         local trigger = self.event_triggers[message.event]
         if trigger then trigger(message) end
     end
+
+    self:send('latency', {
+        send_time = socket.gettime()
+    })
 end
 
 function Client:on(event, trigger)
     self.event_triggers[event] = trigger
+end
+
+function Client:get_latency()
+    return self.latency
 end
 
 function Client:connect()
